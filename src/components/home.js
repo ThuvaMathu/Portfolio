c462a04d2d87ec000fb275db0cf9555dae8217ea
shankar19021998@gmail.com <shankar19021998@gmail.com>
2022-01-31 22:13:56 +0530

    carousel home

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 export default function Home() {
+  const settings = {
+    dots: false,
+    infinite: false,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
-    <>
-<Image src={require("../assets/home.jpg")} style={{width:'100%',height:'700px'}}></Image>
-    </>
+    <div >
+    <Slider{...settings}>
+
+    <Image src={require("../assets/image4.jpg")} style={{ width: '100%',  }}></Image>
+    <Image src={require("../assets/image5.png")} style={{ width: '100%',  }}></Image>
+    <Image src={require("../assets/image4.jpg")} style={{ width: '100%',}}></Image>
+    </Slider>
+
+    </div>
     
   );
 }
